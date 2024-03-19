59cf5fbad39278090286ec99a47ec0073dc5b052
sampath04141 <sam.auck2@gmail.com>
2024-03-20 10:36:33 +1300

    removed firefox and webkit

---
+++ README.md
@@ -0,0 +1,6 @@
+Playwright project with Javascript
+followed page object modle
+added allure report, junit and json
+run parall
+support multiple browsers
+lined with Assure pipeline
--- playwright.config.js
+++ playwright.config.js
@@ -41,21 +41,21 @@ module.exports = defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
-    {
-      name: 'Mobile Chrome',
-      use: { ...devices['iPhone 12'] },
-    },
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['iPhone 12'] },
+    // },
     // {
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
