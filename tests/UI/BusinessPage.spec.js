90406dd08e56a23a4648731b957772ddea0b394f
sampath04141 <sam.auck2@gmail.com>
2024-04-07 17:57:25 +1200

    Parameterized data via Json

--- tests/UI/BusinessPage.spec.js
+++ tests/UI/BusinessPage.spec.js
@@ -2,10 +2,11 @@ import { HomePage } from "../../Pages/HomePage";
 
 import { test, expect } from "@playwright/test";
 import { BusinessPage } from "../../Pages/BusinessPage";
+const testData = require('../../TestInputData/data.json');
 
 
-var home
-var business
+let home
+let business
 
 test.beforeEach(async ({ page }) => {
     await page.goto('/', { timeout: 60000 })
@@ -18,28 +19,29 @@ test.afterEach(async ({ page }) => {
     await page.screenshot({ path: 'test-results/screenshots/' + Date.now() + '-screenshot.png' })
 })
 
+
 test('validate Business special table', async ({ page }) => {
-    const boxTitle = 'Special'
+    const boxTitle = testData.term_block.boxTitle
     await business.scrollToPromo()
     expect(await business.getText(boxTitle)).toEqual(boxTitle)
 
 })
 
 test('Validate Card type', async ({ page }) => {
-    const cardType = 'Fixed term'
+    const cardType =testData.term_block.cardType
     await business.scrollToPromo()
     expect(await business.getText(cardType)).toEqual(cardType)
 
 })
 
 test('Validate Rate', async ({ page }) => {
-    const term = 'Deposit'
+    const term = testData.term_block.term
     await business.scrollToPromo()
     expect(await business.getText(term)).toContain('Term Deposit (1 year)')
 })
 
 test('Validate Sub Heading', async ({ page }) => {
-    const blurb = 'Blurb'
+    const blurb = testData.term_block.blurb
     await business.scrollToPromo()
     expect(await business.getText(blurb)).toEqual('Fixed returns on money locked in for a set period.')
 
